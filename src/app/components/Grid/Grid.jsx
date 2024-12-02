ad25a30ac735c20d43ff841195cda594785b50a1
AakashKathait <92093691+AakashKathait@users.noreply.github.com>
2024-12-01 21:49:36 -0800

    ch

--- src/app/components/Grid/Grid.jsx
+++ src/app/components/Grid/Grid.jsx
@@ -20,7 +20,7 @@ const Grid = () => {
   };
 
   const images = [];
-  for (let i = 1; i <= 42; i++) {
+  for (let i = 1; i <= 37; i++) {
     images.push(require(`../../assets/PICS/image (${i}).jpg`));
   }
   return (
